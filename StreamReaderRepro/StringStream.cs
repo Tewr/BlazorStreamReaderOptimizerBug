ca85e039ffccfdbbf883f3a20a4319654fbb798c
Tor KNUTSSON <Tewr@users.noreply.github.com>
2020-06-05 20:04:22 +0200

    initial

---
+++ StreamReaderRepro/StringStream.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StreamReaderRepro
+{
+    public class StringStream : Stream
+    {
+        private byte[] _bytes;
+
+        public StringStream(string source, Encoding encoding)
+        {
+            _bytes = encoding.GetBytes(source);
+        }
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => true;
+
+        public override bool CanWrite => false;
+
+        public override long Length => _bytes.Length;
+
+        public override long Position { get; set; }
+
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            if (offset > Length)
+                throw new ArgumentOutOfRangeException("offset");
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    {
+                        if (offset < 0)
+                            throw new IOException("SeekBeforeBegin");
+                        Position = offset;
+                        break;
+                    }
+                case SeekOrigin.Current:
+                    {
+                        int tempPosition = unchecked((int)Position + (int)offset);
+                        if (unchecked((int)Position + offset) < 0 || tempPosition < 0)
+                            throw new IOException("SeekBeforeBegin");
+                        Position = tempPosition;
+                        break;
+                    }
+                case SeekOrigin.End:
+                    {
+                        int tempPosition = unchecked((int)(Length + (int)offset));
+                        if (unchecked(Length + offset) < 0 || tempPosition < 0)
+                            throw new IOException("IO.IO_SeekBeforeBegin");
+                        Position = tempPosition;
+                        break;
+                    }
+                default:
+                    throw new ArgumentException("Argument_InvalidSeekOrigin");
+            }
+
+            return Position;
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            await Task.Delay(1);
+            var actualCount = count;
+            if (Position + actualCount > Length)
+            {
+                actualCount = (int)(Length - Position);
+            }
+            Array.Copy(_bytes, Position, buffer, offset, actualCount);
+
+            Position += actualCount;
+
+            return actualCount;
+            //return base.ReadAsync(buffer, offset, count, cancellationToken);
+        }
+    }
+}
